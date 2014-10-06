084819396a1a65bd42e7d6612da891a75bae1bc5
yinzelin <yinzelin@yinzelindeMacBook-Pro.local>
2014-10-06 10:30:26 +0800

    Initial Commit

---
+++ AsingDemo1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AsingDemo1
+//
+//  Created by yinzelin on 14-10-6.
+//  Copyright (c) 2014年 ___YUEDUZHE___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AsingDemo1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AsingDemo1
+//
+//  Created by yinzelin on 14-10-6.
+//  Copyright (c) 2014年 ___YUEDUZHE___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
